cf992e9abd70851073680608dcaf533abe9f99aa
Abhishek Kumar Singh <abhishek800254@gmail.com>
2026-05-13 08:19:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-backend-x235.onrender.com"
+export const ServerUrl  = "https://ai-mock-interview-z1dw.onrender.com"
 
 function App() {
 
